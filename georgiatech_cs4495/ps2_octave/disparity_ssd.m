87216f24646db171fe3910210a36cb2d5591a24e
unknown <andrewf.wang@goertek.com>
2016-11-16 13:34:41 +0800

    update disparity ssd

--- georgiatech_cs4495/ps2_octave/disparity_ssd.m
+++ georgiatech_cs4495/ps2_octave/disparity_ssd.m
@@ -7,33 +7,26 @@ function D = disparity_ssd(L, R)
 
     % TODO: Your code here
     [h, w] = size(L);
-    win_w = 3;
-    win_h = 3;
+    win_w = 11;
+    win_h = 11;
     
-    patch = zeros(win_h, win_w);
-    strip = zeros(win_h, w);
     D = zeros(h, w);
 
     for i = 1 : h - win_h + 1
         for j = 1 : w - win_w + 1
             % abstract a window from L
-            patch = L(i : i + win_h - 1, j : j + win_w - 1);
-            strip = R(i : i + win_h - 1, :);
-            best_x = find_best_match(patch, strip);
-            D(i, j) = best_x - j;
+            D(i, j) = find_best_match(L(i : i + win_h - 1, j : j + win_w - 1),...
+                            R(i : i + win_h - 1, :)) - j;
         end
     end
 end
 
-function best_x = find_best_match(patch, strip)
-    min_diff = Inf;
-    best_x = 1;
-    
+function best_x = find_best_match(patch, strip)    
+
+    diff = zeros(size(strip, 2) - size(patch, 2) + 1);
     for i = 1 : size(strip, 2) - size(patch, 2) + 1
-        diff = sumsq((patch - strip(:, i : i + size(patch, 2) - 1))(:));
-        if(diff < min_diff)
-            min_diff = diff;
-            best_x = i;
-        end
+        diff(i) = sumsq((patch - strip(:, i : i + size(patch, 2) - 1))(:));
     end
+    
+    best_x = find(diff == min(diff))(1);
 end
--- georgiatech_cs4495/ps2_octave/ps2.m
+++ georgiatech_cs4495/ps2_octave/ps2.m
@@ -1,6 +1,11 @@
 % ps2
+clc;
+clear all;
+close all;
+
 pkg load image;  % Octave only
 
+%{
 %% 1-a
 % Read images
 L = im2double(imread(fullfile('input', 'pair0-L.png')));
@@ -9,12 +14,20 @@ R = im2double(imread(fullfile('input', 'pair0-R.png')));
 % Compute disparity
 D_L = disparity_ssd(L, R);
 D_R = disparity_ssd(R, L);
+
+% TODO: Save output images (D_L as output/ps2-1-a-1.png and D_R as 
+% output/ps2-1-a-2.png)
+% Note: They may need to be scaled/shifted before saving to show results 
+% properly
 figure, imshow(D_L, []);
 print('output/ps2-1-a-1.png');
 figure, imshow(D_R, []);
 print('output/ps2-1-a-2.png');
-
-% TODO: Save output images (D_L as output/ps2-1-a-1.png and D_R as output/ps2-1-a-2.png)
-% Note: They may need to be scaled/shifted before saving to show results properly
+%}
 
 % TODO: Rest of your code here
+L = im2double(rgb2gray(imread('input/pair1-L.png')));
+R = im2double(rgb2gray(imread('input/pair1-R.png')));
+D_L = disparity_ssd(L, R);
+figure, imshow(D_L, []);
+print('output/ps2-2-a-1.png')
