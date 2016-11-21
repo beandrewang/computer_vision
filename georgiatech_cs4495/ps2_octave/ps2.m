67844f586b859197f2fa06b7013819ff785768ef
unknown <andrewf.wang@goertek.com>
2016-11-21 11:49:03 +0800

    add ps3

--- georgiatech_cs4495/ps2_octave/ps2.m
+++ georgiatech_cs4495/ps2_octave/ps2.m
@@ -5,7 +5,7 @@ close all;
 
 pkg load image;  % Octave only
 
-%{
+
 %% 1-a
 % Read images
 L = im2double(imread(fullfile('input', 'pair0-L.png')));
@@ -13,7 +13,7 @@ R = im2double(imread(fullfile('input', 'pair0-R.png')));
 
 % Compute disparity
 D_L = disparity_ssd(L, R);
-D_R = disparity_ssd(R, L);
+%D_R = disparity_ssd(R, L);
 
 % TODO: Save output images (D_L as output/ps2-1-a-1.png and D_R as 
 % output/ps2-1-a-2.png)
@@ -21,13 +21,18 @@ D_R = disparity_ssd(R, L);
 % properly
 figure, imshow(D_L, []);
 print('output/ps2-1-a-1.png');
-figure, imshow(D_R, []);
-print('output/ps2-1-a-2.png');
-%}
+%figure, imshow(D_R, []);
+%print('output/ps2-1-a-2.png');
 
+
+%{
 % TODO: Rest of your code here
 L = im2double(rgb2gray(imread('input/pair1-L.png')));
 R = im2double(rgb2gray(imread('input/pair1-R.png')));
 D_L = disparity_ssd(L, R);
 figure, imshow(D_L, []);
-print('output/ps2-2-a-1.png')
+print('output/ps2-2-a-1.png');
+D_R = disparity_ssd(R, L);
+figure, imshow(D_R, []);
+print('output/ps2-2-a-2.png');
+%}
